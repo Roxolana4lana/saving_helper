e4d10dc2061ebb3de92ca53a267b39e919ee3bf1
Roxolana <roxolanapavlyk@gmail.com>
2019-01-24 11:04:53 +0100

    added expenses form

---
+++ app/src/components/ExpenseForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ExpenseForm extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default  ExpenseForm
