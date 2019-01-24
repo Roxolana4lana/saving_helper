8ed2bf7a92c61179bbac2ffe807f4b3b0fecc8a9
Roxolana <roxolanapavlyk@gmail.com>
2019-01-24 13:56:04 +0100

    added a component for dislaying the whole list of expenses

--- app/src/components/expenses.js
+++ app/src/components/expenses.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react'
+import ExpenseForm from './ExpenseForm'
 
-export default class Expenses extends Component {
+
+class Expenses extends Component {
+  constructor(){
+    super()
+    this.state={
+      expenses:[]
+    }
+  }
+  componentDidMount(){
+    fetch('http://127.0.0.1:8000/expenses/')
+    .then(res=>res.json())
+    .then(data=>{
+      this.setState({
+        expenses: data
+      })
+    })
+  }
   render() {
+    const expenseList= this.state.expenses.map(expense=>(
+      <div className="expense_list_item" key={expense.id}>
+      <h3>{expense.title}</h3>
+      </div>
+    ))
     return (
-      <div>
-        
+      <div className='App'>
+      <div className="App__Expenses">
+      <h1>List of expenses</h1>
+      <div className="expenses_lising">
+      {expenseList}
+        </div>
+        </div>
+        <div className="App_Form">
+        <ExpenseForm/>
+        </div>
+     
       </div>
     )
   }
 }
+export default Expenses
