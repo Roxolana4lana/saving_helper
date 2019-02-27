901703d0a4ee96cada4e5cd17f4c02ccfd2ea1e2
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 20:23:52 +0100

    clean up in component

--- app/src/components/ExpenseOne.js
+++ app/src/components/ExpenseOne.js
@@ -16,30 +16,29 @@ class ExpenseOne extends Component {
   componentDidMount(){
     let id=this.props.match.params.exact;
   axios.get('http://127.0.0.1:8000/expenses/' + id + '/')
-    .then(res => this.setState({ expense: res.data }))
-     
+    .then(res => this.setState({ expense: res.data })) 
   }
-  SeeDraft=()=>{
+
+  seeDraft=()=>{
  this.setState(currentState =>{
    return{
-checked:!currentState.checked
-   }
+          checked:!currentState.checked
+         }
  })}
 
   render() {
+
   const see =   this.state.checked ? (<div> <Drafty /></div>):null
- console.log(this.state.expense.title)
     return (
       <div className="ExpensOne">
       <div >
           <h2 className="Exp_one_info">Additional information on this expense</h2>
-      <h5>The title of thie expense is {this.state.expense.title}</h5>
-      <p> The amount is {this.state.expense.amount} </p>
+          <h5>The title of thie expense is {this.state.expense.title}</h5>
+          <p> The amount is {this.state.expense.amount} </p>
       </div>
-      <div>
-     
-          <Switch onChange={this.SeeDraft} checked={this.state.checked}></Switch>
-    {see}
+      <div> 
+          <Switch onChange={this.seeDraft} checked={this.state.checked}></Switch>
+          {see}
       </div>
       </div>
     )
