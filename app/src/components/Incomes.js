38ae948e9efe0dbeadd2c5da5cc73c9f34583ee6
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 20:34:23 +0100

    clean up in component

--- app/src/components/Incomes.js
+++ app/src/components/Incomes.js
@@ -11,36 +11,32 @@ class Incomes extends Component {
                 incomes:[]
             }
         }
+
     componentWillMount(){
-        axios.get('http://127.0.0.1:8000/incomes/')
-        
-     
+        axios.get('http://127.0.0.1:8000/incomes/')   
             .then(res => this.setState({ incomes: res.data }))
             .catch(err => (console.log(err)))
     }
-  render() {
-      const incomes=this.state.incomes.map(income=>(
-          <div key={income.id}>
-              <div className="element_list">
-              <Link to={'/profit' + income.id} className="link_expense">
-          <span>{income.from_where}
-                  </span></Link>
-              <IncomeDelete id={income.id} />
-          </div>
-          </div>))
-    return (
-        <div className='App'>
-           
 
+    render() {
+        const incomes=this.state.incomes.map(income=>(
+            <div key={income.id}>
+                <div className="element_list">
+                    <Link to={'/profit' + income.id} className="link_expense">
+                        <span>{income.from_where}</span>
+                    </Link>
+                    <IncomeDelete id={income.id} />
+                </div>
+            </div>))
+    return (
+        <div className='App'>       
             <div className="split expenses_lising">
-            <h1>Incomes</h1>
-                    {incomes}
-               
+                <h1>Incomes</h1>
+                {incomes}
             </div>
             <div className="split App_Form">
                <IncomesForm/>
             </div>
-
         </div>
     )
   }
