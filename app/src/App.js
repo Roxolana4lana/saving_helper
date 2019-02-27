c1e7117f660007d1669b0d63063c06897738cdd4
Roxolana <roxolanapavlyk@gmail.com>
2019-02-27 19:48:32 +0100

    import components to root component

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Expenses from './components/expenses'
+import ExpenseOne from './components/ExpenseOne';
+import Drafty from './components/Drafty'
+import Incomes from './components/Incomes'
+import IncomeOne from './components/IncomeOne';
+import './output.css'
+
 
 class App extends Component {
   render() {
